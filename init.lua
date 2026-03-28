837f60d9dc2ce394a2d14fed9e4fc577681b501d
kedesiklem <marin@elie.org>
2026-03-28 05:34:21 +0100

    1.1.1 - fix : dup increase timer entity

--- init.lua
+++ init.lua
@@ -3,7 +3,19 @@ dofile_once( "mods/noFireTimer/files/lib/variables.lua");
 
 function OnPlayerSpawned( player_entity )
     EntityLoad( "mods/noFireTimer/files/gui/container.xml" );
-    EntityAddComponent( player_entity, "LuaComponent", {
-        script_source_file = "mods/noFireTimer/files/scripts/fire_detection.lua"
-    });
+    local luas_player = EntityGetComponentIncludingDisabled(player_entity, "LuaComponent")
+    local need_skip = false
+    if luas_player then
+        for _, lua in pairs(luas_player) do
+            if ComponentGetValue2(lua, "script_source_file") == "mods/noFireTimer/files/scripts/fire_detection.lua" then
+                need_skip = true
+            end
+        end
+    end
+    if not need_skip then
+        EntityAddComponent( player_entity, "LuaComponent", {
+            script_source_file = "mods/noFireTimer/files/scripts/fire_detection.lua"
+        });
+    end
+
 end
